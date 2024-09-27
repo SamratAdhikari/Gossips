73fddcf71ce8708cc97fa5b820eb848d0ca0244d
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-09-27 23:35:09 +0545

    render

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import connectDB from "./db/connect.to.db.js";
 
@@ -10,6 +11,8 @@ import { app, server } from "./socket/socket.js";
 
 // constants
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 await connectDB();
 
 app.use(express.json());
@@ -19,6 +22,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// middleware
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     console.log(`App running at port ${PORT} ...`);
